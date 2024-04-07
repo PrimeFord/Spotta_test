67398251b26f1cdd39b3f088cd088c825837b7a7
Prime Senpai <Bashfordbash@gmail.com>
2024-04-07 16:59:30 +0100

    feat: readMe

---
+++ README.md
@@ -0,0 +1,56 @@
+# Spotta
+
+Spotta: This project is a brief test on the deployment and best practice method of a real estate web app.
+
+## Getting Started
+
+Clone or download this repository and run with a local server of open `localhost:5173/` with your favorite browser
+
+## Prequisite
+
+- Your browser version must be recent for a better experience.
+
+## Features
+
+- Home Page
+- Review Page
+- Create Review Modal
+
+## Tech/framework used
+
+- HTML
+- CSS
+- ReactJS
+- Vite
+- Tailwind-CSS
+- TypeScript
+
+## Package Manager
+- NPM
+
+
+## Preview
+
+[Preview Here](https://spotta-test.vercel.app/)
+
+## Display on both Laptop and Mobile Phone
+![screenshot](./public/Image/snip.png)
+![screenshot](./public/Image/snip2.png)
+![screenshot](./public/Image/snip3.png)
+![screenshot](./public/Image/snip4.png)
+![screenshot](./public/Image/snip5.png)
+![screenshot](./public/Image/snip6.png)
+![screenshot](./public/Image/snip7.png)
+![screenshot](./public/Image/snip8.png)
+![screenshot](./public/Image/snip9.png)
+![screenshot](./public/Image/snip10.png)
+![screenshot](./public/Image/snip11.png)
+![screenshot](./public/Image/snip12.png)
+
+## Contact
+
+If you want to contact me you can reach me at
+Email - wolabash@gmail.com 
+Github- https://github.com/PrimeFord
+
+
--- src/pages/SearchResult/index.tsx
+++ src/pages/SearchResult/index.tsx
@@ -3,16 +3,17 @@ import ReviewCard from "../../components/review";
 import ModalContainer from "../../layouts/ModalContainer";
 import { useModalContext } from "../../components/Context/modal";
 import { useAddressContext } from "../../components/Context/address";
-// import { IoIosStar } from "react-icons/io";
 import Ratings from "../../components/Rating";
 import { useState } from "react";
 import { BiChevronDown } from "react-icons/bi";
+import { IoCheckmarkDoneCircleOutline } from "react-icons/io5";
 
 const SearchAddress = () => {
   const { show, setShow } = useModalContext();
   const { address } = useAddressContext();
   const [hide, setHide] = useState(false);
   const [input, setInput] = useState("");
+  const [done, setDone] = useState(false);
   // const address = localStorage.getItem("addressState");
   const handleHide = (e: any) => {
     if (e.target.id === "drop") {
@@ -49,124 +50,157 @@ const SearchAddress = () => {
         />
       </div>
       <ModalContainer isVisible={show} onClose={() => setShow(false)}>
-        <div id="drop" onClick={handleHide}>
-          <p className="text-[1rem] lg:text-[1.2rem] font-[400] text-center mb-2">
-            Review Location
-          </p>
-          <p className="text-[1rem] mb-2 lg:text-[1.2rem] font-[500]">
-            {address || "Bonny and Clyde Street, Ajao Estate, Lagos."}
-          </p>
-          <form
-            action=""
-            //  onSubmit={() => {}}
-          >
-            <label htmlFor="amenities" className="relative">
-              <div className="w-[100%] h-[2.8rem] rounded-[8px] flex flex-row-reverse items-center bg-[#F3F7FE] border-[1px] border-solid border-[#D4DCF1] px-3">
-                <span className="pr-3">
-                  <BiChevronDown
-                    style={{
-                      height: "1.5rem",
-                      width: "1.5rem",
-                      color: "#0D2159",
-                    }}
+        {!done ? (
+          <div id="drop" onClick={handleHide}>
+            <p className="text-[1rem] lg:text-[1.2rem] font-[400] text-center mb-2">
+              Review Location
+            </p>
+            <p className="text-[1rem] mb-2 lg:text-[1.2rem] font-[500]">
+              {address || "Bonny and Clyde Street, Ajao Estate, Lagos."}
+            </p>
+            <form
+              action=""
+              //  onSubmit={() => {}}
+            >
+              <label htmlFor="amenities" className="relative">
+                <div className="w-[100%] h-[2.8rem] rounded-[8px] flex flex-row-reverse items-center bg-[#F3F7FE] border-[1px] border-solid border-[#D4DCF1] px-3">
+                  <span className="pr-3">
+                    <BiChevronDown
+                      style={{
+                        height: "1.5rem",
+                        width: "1.5rem",
+                        color: "#0D2159",
+                      }}
+                      onClick={() => {
+                        setHide(!hide);
+                      }}
+                    />
+                  </span>
+                  <input
+                    className="w-[100%] bg-[#F3F7FE] placeholder:text-[#484851] outline-0 border-0"
+                    required
+                    type="text"
+                    name="search"
+                    placeholder="Select Amenities"
+                    autoComplete="off"
                     onClick={() => {
                       setHide(!hide);
                     }}
+                    id="search"
                   />
-                </span>
-                <input
-                  className="w-[100%] bg-[#F3F7FE] placeholder:text-[#484851] outline-0 border-0"
+                </div>
+                <div
+                  className={` ${
+                    hide ? "grid" : "hidden"
+                  } h-[15rem] lg:h-[10rem] w-full lg:w-[100%] absolute bg-[#F3F7FE] p-1 grid grid-cols-3 lg:grid-cols-5 border-[1px] border-solid border-[#D4DCF1] rounded-[8px]`}
+                >
+                  {reviewTags.map((e, i) => (
+                    <label
+                      key={i}
+                      htmlFor={e}
+                      className="text-[0.7rem] lg:text-[0.8rem] font-[400] flex items-center gap-2 text-[#484851]"
+                    >
+                      <input
+                        id={e}
+                        name={e}
+                        value={e}
+                        type="checkbox"
+                        className="h-4 w-4 rounded bg-[#F3F7FE] border-[#D4DCF1] "
+                      />
+                      {e}
+                    </label>
+                  ))}
+                </div>
+              </label>
+
+              <label htmlFor="review" className="flex flex-col gap-2 mt-4">
+                <p className="text-[0.85rem] lg:text-[0.9rem] font-[400]">
+                  Rate Location
+                </p>
+                <Ratings />
+              </label>
+
+              <label htmlFor="review" className="flex flex-col gap-2 mt-4">
+                <p className="text-[0.85rem] lg:text-[0.9rem] font-[400]">
+                  Write Review
+                </p>
+                <textarea
                   required
-                  type="text"
-                  name="search"
-                  placeholder="Select Amenities"
-                  autoComplete="off"
-                  onClick={() => {
-                    setHide(!hide);
+                  id="about"
+                  name="about"
+                  rows={4}
+                  className="block w-full mt-0 rounded-md border-0 p-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-0 sm:text-sm sm:leading-6"
+                  value={input}
+                  onChange={(e) => {
+                    e.preventDefault();
+                    setInput(e.target.value);
                   }}
-                  id="search"
+                  placeholder="Write your review here"
                 />
-              </div>
-              <div
-                className={` ${
-                  hide ? "grid" : "hidden"
-                } h-[15rem] lg:h-[10rem] w-full lg:w-[100%] absolute bg-[#F3F7FE] p-1 grid grid-cols-3 lg:grid-cols-5 border-[1px] border-solid border-[#D4DCF1] rounded-[8px]`}
+              </label>
+
+              <label
+                htmlFor="candidates"
+                className="text-[0.85rem] lg:text-[0.9rem] font-[400] mt-4 flex items-center gap-2 text-[#484851]"
               >
-                {reviewTags.map((e, i) => (
-                  <label
-                    key={i}
-                    htmlFor={e}
-                    className="text-[0.7rem] lg:text-[0.8rem] font-[400] flex items-center gap-2 text-[#484851]"
-                  >
-                    <input
-                      id={e}
-                      name={e}
-                      value={e}
-                      type="checkbox"
-                      className="h-4 w-4 rounded bg-[#F3F7FE] border-[#D4DCF1] "
-                    />
-                    {e}
-                  </label>
-                ))}
+                <input
+                  id="isAnonymous"
+                  name="anonymous"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 "
+                />
+                Post as Anonymous
+              </label>
+              <div className="flex gap-6 mt-4">
+                <button
+                  type="button"
+                  disabled={input !== "" ? false : true}
+                  className="bg-[#3366FF] disabled:bg-[#3366ff50] w-[38rem] text-[#fff] hover:bg-[#3366FF95] text-[0.9rem] rounded-[8px] p-2"
+                  onClick={() => setDone(true)}
+                >
+                  SUBMIT
+                </button>
+                <button
+                  className="border-[#3366FF] w-[38rem] text-[#3366FF] hover:bg-[#5378F620] border-solid border-[1px] text-[0.9rem] p-2 rounded-[8px]"
+                  onClick={() => setShow(false)}
+                >
+                  CANCEL
+                </button>
               </div>
-            </label>
-
-            <label htmlFor="review" className="flex flex-col gap-2 mt-4">
-              <p className="text-[0.85rem] lg:text-[0.9rem] font-[400]">
-                Rate Location
-              </p>
-              <Ratings />
-            </label>
-
-            <label htmlFor="review" className="flex flex-col gap-2 mt-4">
-              <p className="text-[0.85rem] lg:text-[0.9rem] font-[400]">
-                Write Review
-              </p>
-              <textarea
-                required
-                id="about"
-                name="about"
-                rows={4}
-                className="block w-full mt-0 rounded-md border-0 p-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-0 sm:text-sm sm:leading-6"
-                value={input}
-                onChange={(e) => {
-                  e.preventDefault();
-                  setInput(e.target.value);
-                }}
-                placeholder="Write your review here"
-              />
-            </label>
-
-            <label
-              htmlFor="candidates"
-              className="text-[0.85rem] lg:text-[0.9rem] font-[400] mt-4 flex items-center gap-2 text-[#484851]"
+            </form>
+          </div>
+        ) : (
+          <div
+            id="drop"
+            className=" text-center flex mt-5 flex-col items-center "
+            onClick={handleHide}
+          >
+            <p className="text-[1rem] lg:text-[1.2rem] font-[400] text-center mb-2">
+              Review Location
+            </p>
+            <p className="text-[1rem] mb-2 lg:text-[1.2rem] font-[500]">
+              Review has been sucessfully sent!
+            </p>
+            <IoCheckmarkDoneCircleOutline
+              style={{
+                height: "10rem",
+                width: "10rem",
+                color: "#3366FF",
+                textAlign: "center",
+              }}
+            />
+            <button
+              type="button"
+              className="bg-[#3366FF] w-full text-[#fff] hover:bg-[#3366FF95] text-[0.9rem] mt-2 rounded-[8px] p-2"
+              onClick={() => {
+                setDone(false);
+                setShow(false);
+              }}
             >
-              <input
-                id="isAnonymous"
-                name="anonymous"
-                type="checkbox"
-                className="h-4 w-4 rounded border-gray-300 "
-              />
-              Post as Anonymous
-            </label>
-            <div className="flex gap-6 mt-4">
-              <button
-                type="button"
-                // disabled={input !== "" ? false : true}
-                disabled={true}
-                className="bg-[#3366FF] disabled w-full text-[#fff] hover:bg-[#3366FF95] text-[0.9rem] rounded-[8px] p-2"
-              >
-                SUBMIT
-              </button>
-              <button
-                className="border-[#3366FF] w-[38rem] text-[#3366FF] hover:bg-[#5378F620] border-solid border-[1px] text-[0.9rem] p-2 rounded-[8px]"
-                onClick={() => setShow(false)}
-              >
-                CANCEL
-              </button>
-            </div>
-          </form>
-        </div>
+              Done
+            </button>
+          </div>
+        )}
       </ModalContainer>
     </div>
   );
